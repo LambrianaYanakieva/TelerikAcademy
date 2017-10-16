3a3de66a8e077f8c9856ec600f0e0eaa70dd5305
LambrianaYanakieva <lambrianayanakieva@gmail.com>
2017-10-17 00:45:01 +0300

    homework added

---
+++ C#/01.Fundamentals/02.Data-Types-And-Variables/ComparingFloats/ComparingFloats/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingFloats
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var firstNumber = double.Parse(Console.ReadLine());
+            var secondNumber = double.Parse(Console.ReadLine());
+            var esp = 0.000001;
+            var result = Math.Abs(firstNumber - secondNumber);
+
+            if(result < esp)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
---
+++ C#/01.Fundamentals/02.Data-Types-And-Variables/PrinttheASCIITable/PrinttheASCIITable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinttheASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 255; i++)
+            {
+                Console.Write("{0}", (char)i);
+            }
+        }
+    }
+}
